d72c7326df3c5314deeab7cbecd6cde4cab07be7
Mitchell Lee <mitch@zyrn.dev>
2025-09-02 17:11:01 +1000

    fixed install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ set -euo pipefail
 grep -v '^#' packages | xargs -r sudo apt install -yq && clear
 
 for dir in `find . -maxdepth 1 -type d -not -path '.' | grep -v '\.git'`; do
-    echo "Stowing ${dir##*/}"
-    # stow --target="$HOME" --restow  "${dir##*/}"
+    stow --target="$HOME" --restow  "${dir##*/}"
 done
