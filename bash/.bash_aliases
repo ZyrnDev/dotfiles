63047b5d2c2b7bdac30a2cf5b71e8cbd525698ec
Mitchell Lee <mitch@zyrn.dev>
2026-01-19 18:34:44 +1100

    more updates my man

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -1,2 +1,10 @@
 alias zr='zentr'
 alias zl='zellij-layout'
+
+alias xc='xclip -in -selection clipboard'
+alias xp='xclip -out -selection clipboard'
+
+# Git aliases
+alias gf='git fetch'
+alias gu='git fetch && git merge'
+alias gr='git-rebase'
--- install.sh
+++ install.sh
@@ -2,16 +2,33 @@
 
 set -euo pipefail
 
-sudo mkdir -p /etc/apt/keyrings
-if [ ! -f /etc/apt/keyrings/charm.gpg ] || [ ! -f /etc/apt/sources.list.d/charm.list ]; then
-    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
-    sudo apt update
-fi
+SKIP_PACKAGES=false
 
-grep -v '^#' packages | xargs -r sudo apt install -yq
-curl -fsSL 'https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz' | tar -xz -C ~/.local/bin zellij
-clear
+# handle args
+for arg in "$@"; do
+    case $arg in
+        --skip-packages)
+        SKIP_PACKAGES=true
+        shift
+        ;;
+        *)
+        gum log -l fatal "Unknown argument: $arg"
+        ;;
+    esac
+done
+
+if [ "$SKIP_PACKAGES" = false ]; then
+    sudo mkdir -p /etc/apt/keyrings
+    if [ ! -f /etc/apt/keyrings/charm.gpg ] || [ ! -f /etc/apt/sources.list.d/charm.list ]; then
+        curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+        sudo apt update
+    fi
+
+    grep -v '^#' packages | xargs -r sudo apt install -yq
+    curl -fsSL 'https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz' | tar -xz -C ~/.local/bin zellij
+    curl -fsSL 'https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage' -o ~/.local/bin/nvim && chmod +x ~/.local/bin/nvim
+fi
 
 for dir in `find . -maxdepth 1 -type d -not -path '.' | grep -v '\.git'`; do
     stow --target="$HOME" --restow  "${dir##*/}"
