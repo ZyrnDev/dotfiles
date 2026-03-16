61277ee700c59939517fb9cf225efd5fd767613e
Mitchell Lee <mitch@zyrn.dev>
2026-03-16 20:42:52 +1100

    add ipmi-boot

--- scripts/.local/bin/common.sh
+++ scripts/.local/bin/common.sh
@@ -10,10 +10,56 @@ for dependency in gum; do
 done
 
 function log() {
+    if [ $# -lt 1 ]; then
+        log fatal 'log requires at least one argument'
+    fi
     local level="$1"
-    shift;
+    shift
     gum log --time rfc822 --structured --level "$level" "$@"
 }
+
+function prompt_choice() {
+    if [ $# -lt 1 ]; then
+        log fatal 'prompt_choice requires at least one argument'
+        return 1
+    fi
+    local ID="$1"
+    shift
+    if [ "${HEADLESS:-false}" = true ] || [ ! -v "$ID" ]; then
+        log fatal "environment variable is not set" variable "$ID"
+    elif [ -v "$ID" ]; then
+        grep -xF "${!ID:-}" | head -n 1 \
+          || log fatal "environment variable does not match any of the options" variable "$ID" value "${!ID:-}"
+    else
+        gum choose "$@"
+    fi
+}
+
+
+################################
+## jq helpers
+################################
+#
+#function to-json() {
+#    jq -R '.'
+#}
+#
+#function to-object() {
+#    if [ "$#" -ne 1 ]; then
+#        echo "Usage: to-object <key>" 1>&2
+#        exit 1
+#    fi
+#
+#    jq --arg key "$1" '{($key): .}'
+#}
+#
+#function to-array() {
+#    jq -s '.'
+#}
+#
+#function merge() {
+#    jq -s 'add'
+#}
 
 # # Source - https://stackoverflow.com/a
 # # Posted by c.gutierrez, modified by community. See post 'Timeline' for change history
