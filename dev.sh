2f85e597aa39e911ce62309b78290eac890851b1
Mitchell Lee <mitch@zyrn.dev>
2026-04-15 14:41:26 +1000

    add ares3

--- dev.sh
+++ dev.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./install.sh --skip-packages
-clear
-git-auto-commit
+cat << EOF | column -t -s ' '
+ACTION DIRECTION SOURCE DESTINATION
+ALLOW INBOUND mars5.sdn.unsw.edu.au:* mercury2.canopusnet.dev:3000
+ALLOW INBOUND mercury2.canopusnet.dev:* mars5.sdn.unsw.edu.au:5003
+ALLOW INBOUND mercury2.canopusnet.dev:* mars5.sdn.unsw.edu.au:6667
+ALLOW INBOUND mercury2.canopusnet.dev:* mars5.sdn.unsw.edu.au:8083
+ALLOW INBOUND mercury2.canopusnet.dev:* mars5.sdn.unsw.edu.au:29092
+EOF
+
+# ./install.sh --skip-packages
+# clear
+# git-auto-commit
